9e13a8f79f5bb55b1642240612dd0b8cadfa31b3
shayan <dodgeshayan@gmail.com>
2024-02-04 15:58:03 +0100

    Modified version compatible with varying numbers of input nodes and diverse sample training data.

--- Deep-Learning-for-Beginners-master/ch.2/TestDeltaBatch.m
+++ Deep-Learning-for-Beginners-master/ch.2/TestDeltaBatch.m
@@ -1,4 +1,4 @@
-clear all
+clear
 
 X = [ 0 0 1;
       0 1 1;
@@ -11,16 +11,18 @@ D = [ 0
       1
       1
     ];
-      
-W = 2*rand(1, 3) - 1;
+
+[N,M]=size(X);  
+W = 2*rand(1, M) - 1;
 
 for epoch = 1:40000 
   W = DeltaBatch(W, X, D); 
 end
 
-N = 4;
 for k = 1:N
   x = X(k, :)';
   v = W*x;
-  y = Sigmoid(v)
+  y = Sigmoid(v);
+  disp([' y_',num2str(k),' = ', num2str(y)])
 end
+
